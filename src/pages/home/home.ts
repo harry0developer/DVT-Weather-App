bb49e3f041438c2242e4839fcfff17646eb3f885
harry <maphutisetati@maphutis-mbp.home>
2018-07-18 22:22:21 +0200

    add images based on the temperature/weather

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,6 +16,11 @@ export class HomePage {
   isCloudy: boolean = true;
   isSunny: boolean = false;
   isRainy: boolean = false;
+  imageBG: string = '';
+  temperatureIcon: string = '';
+
+  baseImagesUrl: string = '../../assets/Images/sea_';
+  baseIconsUrl: string = '../../assets/Icons/';
 
   today: any; 
   constructor(
@@ -38,6 +43,35 @@ export class HomePage {
     return Math.round(num);
   }
 
+  setTempTheme(data) {
+    if (data.weather[0].main.toLowerCase() === 'clear') {
+      this.isCloudy = false;
+      this.isRainy = false;
+      this.isSunny = true;
+      this.imageBG = this.baseImagesUrl + 'sunny.png';
+    }
+    else if (data.weather[0].main.toLowerCase() === 'rain') {
+      this.isCloudy = false;
+      this.isRainy = true;
+      this.isSunny = false;
+      this.imageBG = this.baseImagesUrl + 'rainy.png';
+    } else {
+      this.isCloudy = true;
+      this.isRainy = false;
+      this.isSunny = false;
+      this.imageBG = this.baseImagesUrl + 'cloudy.png';
+    }
+  }
+
+  getIconByWeatherDescription(desc: string) {
+    if (desc.toLocaleLowerCase() === 'clear') {
+      return this.baseIconsUrl + 'clear@2x.png'
+    }
+    else if (desc.toLocaleLowerCase() === 'rain') {
+      return this.baseIconsUrl + 'rain@2x.png'
+    }
+      return this.baseIconsUrl + 'partlysunny@2x.png'
+  }
   getWeatherData() {
     let tmpData;
     this.feedback.presentLoadingSpinner('Getting location...');
@@ -49,6 +83,7 @@ export class HomePage {
         tmpData = data;
         this.data = tmpData.list.filter(d => d.dt_txt.endsWith('21:00:00'));
         this.today = this.data[0];
+        this.setTempTheme(this.today);
         console.log(this.data);
       }).catch(error => {
         this.feedback.dismissLoadingSpinner();
